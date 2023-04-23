c8d96d195c4bd9d6f2c346f88df2e25c3744a134
christianwaldmann <waldmann-christian@web.de>
2023-04-23 11:28:40 +0200

    Fix inconsistent project titles

--- locales/de/autocomplete-postgres.js
+++ locales/de/autocomplete-postgres.js
@@ -1,5 +1,5 @@
 export const de = {
-	title: "Autocomplete PostgreSQL",
+	title: "Autocomplete mit PostgreSQL",
 	description:
 		"Projekt zum Austesten von Autovervollständigung für ein Eingabefeld mit PostgreSQL. Die Vorschläge basieren auf einer Substring-Suche. Die dafür notwendige Query wird mit einem Trigram-Index (pg_trgm, gin) effizient umgesetzt.",
 };
